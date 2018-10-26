89dd5b2b6f688326eeb5a0625f8fd884f66b1d16
dnora <dnora@umich.edu>
2018-10-26 11:20:49 -0400

    implemented add

--- rpn.py
+++ rpn.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
+    stack = []
+    tokens = arg.split()
+    
+#stack for calculator
+    #tokenize input
+    #process tokens
+    for token in tokens:
+        try:
+            value = int(token)
+            stack.append(value)
+        except ValueError:
+            val1 = stack.pop()
+            val2 = stack.pop()
+            result = val1 + val2
+            stack.append(result)
+            return stack[0]
     pass
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
+        result = calculate(input("rpn calc> "))
+        print(result)
 
 if __name__ == '__main__':
     main()
